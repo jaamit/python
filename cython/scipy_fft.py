508e4874c0ed34816f98629502e3b6b1379f3b3c
Amit Jain <amit.hpepc@gmail.com>
2017-12-29 16:02:04 +0530

    Update scipy_fft.py

--- cython/scipy_fft.py
+++ cython/scipy_fft.py
@@ -1,1 +1,26 @@
+import sys
+import numpy as np
+from scipy import fftpack
+import cProfile, pstats
+
+n = 4800
+m = 22000
+
+s = np.random.random((n, m))
+X = np.zeros((m, n))
+
+print trs.shape
+pr = cProfile.Profile()
+pr.enable()
+
+for itr in xrange(0, n):
+  X[:,itr] = s[itr]
+  FFTX = fftpack.fft(X[:,itr])
+  IFFTX = np.real(fftpack.ifft(FFTX))
+  
+pr.disable()
+sortby = 'time'
+ps = pstats.Stats(pr, stream=sys.stdout).sort_stats(sortby)
+ps.print_stats()
+
 
