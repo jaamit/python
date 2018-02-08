f9b38b5f00a6524887c4396338979ac32b234deb
Amit Jain <amit.hpepc@gmail.com>
2018-02-08 11:14:58 +0530

    Update general_tips.py
    Merge sorted lists

--- misc/general_tips.py
+++ misc/general_tips.py
@@ -1,3 +1,10 @@
 # Few interview Qs for py
 
-1. Merge 2 lists
+#1. Merge 2 lists
+x = [3, 4, 6, 10, 11, 18]
+y = [1, 5, 7, 12, 13, 19, 21]
+
+x.extend(y)
+z = sorted(x)
+#[1, 3, 4, 5, 6, 7, 10, 11, 12, 13, 18, 19, 21]
+
